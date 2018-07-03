6298497ba75cb79376a28a322b6ada7158307ef2
Nolan <nolan_li@icloud.com>
2018-07-03 21:21:00 +0800

    功能完善

--- MTAppEvaluateExample/ViewController.swift
+++ MTAppEvaluateExample/ViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MTAppEvaluateProtocol {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,32 +20,14 @@ class ViewController: UIViewController {
     }
     
     lazy var appEvaluateMgr: MTAppEvaluateManager = {
-        let mgr = MTAppEvaluateManager(appID: "1257686774", type: MTAppEvaluateType.OutApp, delegate: self)
+        let mgr = MTAppEvaluateManager(appID: "1257686774", type: MTAppEvaluateType.Alert, delegate: self)
         return mgr
     }()
-
 }
 
 extension ViewController {
     @objc fileprivate func handleTapAction() {
         self.appEvaluateMgr.show()
-    }
-}
-
-extension ViewController: MTAppEvaluateProtocol {
-    func MT_AppEvaluateTipAlert(comment: (()->Void)?, like: (()->Void)?) -> UIAlertController {
-        let alert = UIAlertController(title: nil, message: "给个五星好评吧~", preferredStyle: UIAlertControllerStyle.alert)
-        let commentAction = UIAlertAction(title: "我要吐槽", style: UIAlertActionStyle.default) { (action) in
-            comment?()
-        }
-        let waitAction = UIAlertAction(title: "看看再说", style: UIAlertActionStyle.default, handler: nil)
-        let admireAction = UIAlertAction(title: "给好评", style: UIAlertActionStyle.cancel) { (action) in
-            like?()
-        }
-        alert.addAction(commentAction)
-        alert.addAction(waitAction)
-        alert.addAction(admireAction)
-        return alert
     }
 }
 
